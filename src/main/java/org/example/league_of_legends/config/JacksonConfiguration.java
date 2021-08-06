faf4950f84e7164d4c11d4c93f717276233a5a2e
Fernando395 <Fe395rodrigues@gmail.com>
2021-08-06 12:42:57 -0300

    Criado Package config

---
+++ src/main/java/org/example/league_of_legends/config/JacksonConfiguration.java
@@ -0,0 +1,23 @@
+package org.example.league_of_legends.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class JacksonConfiguration {
+
+    private final ObjectMapper objectMapper;
+
+    public JacksonConfiguration(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @PostConstruct
+    public ObjectMapper jacksonObjectMapper() {
+        objectMapper.registerModule(new JsonNullableModule());
+        return objectMapper;
+    }
+}
